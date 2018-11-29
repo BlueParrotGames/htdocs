f724f8aa179460f007ebb487e795daa3b73b2d91
rabitibike112 <30289973+rabitibike112@users.noreply.github.com>
2018-11-29 17:00:47 +0200

    So, added images for the links and stuff and Re-made the InConstruction site from scratch
    Copy of the original is there called "original..." new .css that is used by the site. May the force be with us.

---
+++ OriginalUnderConstruction.php
@@ -0,0 +1,92 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Blue Parrot Games</title>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" type="text/css" href="styles/bootstrap4/bootstrap.min.css">
+
+<link rel="stylesheet" type="text/css" href="plugins/OwlCarousel2-2.2.1/owl.carousel.css">
+
+
+<link rel="stylesheet" type="text/css" href="styles/main_styles.css">
+<link rel="stylesheet" type="text/css" href="styles/responsive.css">
+
+<link rel="stylesheet" href="style/nav.css">
+<link rel="stylesheet" href="style/logo.css">
+<link rel="shortcut icon" href="../resources/bpg_512.ico">
+</head>
+
+<body>
+
+<div class="super_container">
+
+	<!-- Header -->
+
+	<header class="header d-flex flex-row justify-content-end align-items-center">
+
+		<!-- Logo -->
+		<div class="logo_container mr-auto">
+			<div class="logo">
+				<a href="#">Blue Parrot Games</a>
+          <p> Looks like you've reached us at a bad time.. How embarrassing! Please come back later to check out our website. In the meantime, Enjoy this page I guess? </p>
+			</div>
+		</div>
+
+	</header>
+
+	<!-- Hero Slider -->
+
+	<div class="home">
+		<div class="hero_slider_container slider_prlx">
+			<div class="owl-carousel owl-theme hero_slider">
+
+	<!-- Features, also a slider item-->
+<div id="image">
+<img class="background" src="resources/BPG wip.png">
+</div>
+	
+</div>
+</div>
+</div>
+
+  <!-- Footer -->
+
+  <footer class="footer">
+    <div class="container">
+      <div class="row footer_content d-flex flex-sm-row flex-column align-items-center">
+        <div class="col-sm-6 cr text-sm-left text-center">
+          <p>
+            This website, is owned and created by BPG. All rights reserverd <br> For more info, feel free to contact grootloops@blueparrotgames.com or tweet @blueparrotgames
+          </p>
+        </div>
+        <div class="col-sm-6 text-sm-right text-center">
+          <div class="footer_social_container">
+            <ul class="footer_social">
+              <li><a href="https://www.facebook.com/blueparrotgames/"><i class="fab fa-facebook-f trans_300"></i></a></li>
+              <li><a href="https://twitter.com/BlueParrotGames"><i class="fab fa-twitter trans_300"></i></a></li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  </footer>
+
+</div>
+
+<script src="js/jquery-3.2.1.min.js"></script>
+<script src="styles/bootstrap4/popper.js"></script>
+<script src="styles/bootstrap4/bootstrap.min.js"></script>
+<script src="plugins/greensock/TweenMax.min.js"></script>
+<script src="plugins/greensock/TimelineMax.min.js"></script>
+<script src="plugins/scrollmagic/ScrollMagic.min.js"></script>
+<script src="plugins/greensock/animation.gsap.min.js"></script>
+<script src="plugins/greensock/ScrollToPlugin.min.js"></script>
+<script src="plugins/progressbar/progressbar.min.js"></script>
+<script src="plugins/OwlCarousel2-2.2.1/owl.carousel.js"></script>
+<script src="plugins/easing/easing.js"></script>
+<script src="js/custom.js"></script>
+</body>
+
+</html>
--- underconstruction.php
+++ underconstruction.php
@@ -1,95 +1,28 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
+<html lang="en">  <head>
 <title>Blue Parrot Games</title>
-<meta charset="utf-8">
-<meta http-equiv="X-UA-Compatible" content="IE=edge">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="stylesheet" type="text/css" href="styles/bootstrap4/bootstrap.min.css">
-<link href="plugins/fontawesome-free-5.0.1/css/fontawesome-all.css" rel="stylesheet" type="text/css">
-<link rel="stylesheet" type="text/css" href="plugins/OwlCarousel2-2.2.1/owl.carousel.css">
-<link rel="stylesheet" type="text/css" href="plugins/OwlCarousel2-2.2.1/owl.theme.default.css">
-<link rel="stylesheet" type="text/css" href="plugins/OwlCarousel2-2.2.1/animate.css">
-<link rel="stylesheet" type="text/css" href="styles/main_styles.css">
+<link rel="stylesheet" type="text/css" href="styles/Under_construction.css">
 <link rel="stylesheet" type="text/css" href="styles/responsive.css">
-
-<link rel="stylesheet" href="style/nav.css">
-<link rel="stylesheet" href="style/logo.css">
-<link rel="shortcut icon" href="../resources/bpg_512.ico">
 </head>
-
 <body>
-
-<div class="super_container">
-
 	<!-- Header -->
 
-	<header class="header d-flex flex-row justify-content-end align-items-center">
+	<header class="header">
 
 		<!-- Logo -->
-		<div class="logo_container mr-auto">
-			<div class="logo">
-				<a href="#">Blue Parrot Games</a>
-          <p> Looks like you've reached us at a bad time.. How embarrassing! Please come back later to check out our website. In the meantime, Enjoy this page I guess? </p>
+		<div class="logo_container">
+		  <a href="#" style="text-decoration:none"><p class="HeaderLinkText">Blue Parrot Games</p></a>
+          <p class="HeaderText"> Looks like you've reached us at a bad time.. How embarrassing! Please come back later to check out our website. In the meantime, Enjoy this page I guess? </p>
 			</div>
 		</div>
 
 	</header>
+    <div id="image">
+         <img class="background" src="resources/BPG wip.png">
+    </div>
 
-	<!-- Hero Slider -->
-
-	<div class="home">
-		<div class="hero_slider_container slider_prlx">
-			<div class="owl-carousel owl-theme hero_slider">
-
-	<!-- Features, also a slider item-->
-
-	<div class="features">
-		<div class="container">
-			<div class="row align-items-end">
-
-				<!-- Features Item -->
-				<div class="col-lg-4 features_col">
-					<div class="features_item d-flex flex-column align-items-center justify-content-end text-center">
-						<div class="icon_container d-flex flex-column justify-content-end">
-							<img src="images/icon_1.svg" alt="">
-						</div>
-						<h3>multi-language use</h3>
-						<p>This website supports a mixture of languages, creating a more stable and powerful environment</p>
-					</div>
-				</div>
-
-        <!-- Features Item -->
-        <div class="col-lg-4 features_col">
-          <div class="features_item d-flex flex-column align-items-center justify-content-center text-center">
-            <div class="icon_container d-flex flex-column justify-content-end">
-              <img src="images/icon_3.svg" alt="">
-            </div>
-            <h3>Optimised product management</h3>
-            <p>We make sure to optimise all of our products to the max, meshes and programming are both taken through a process to determine whether it can be optimised further or not.</p>
-          </div>
-        </div>
-
-				<!-- Features Item -->
-				<div class="col-lg-4 features_col">
-					<div class="features_item d-flex flex-column align-items-center justify-content-center text-center">
-						<div class="icon_container d-flex flex-column justify-content-end">
-							<img src="images/icon_2.svg" alt="">
-						</div>
-						<h3>A bunch of freelancers.</h3>
-						<p>Our team works together, and has worked together for months, if not years. We're used to working together, but not afraid of working alone. </p>
-					</div>
-				</div>
-			</div>
-		</div>
-	</div>
-</div>
-</div>
-</div>
-
-  <!-- Footer -->
-
-  <footer class="footer">
+	  <!-- Footer -->
+	
+	  <footer class="footer">
     <div class="container">
       <div class="row footer_content d-flex flex-sm-row flex-column align-items-center">
         <div class="col-sm-6 cr text-sm-left text-center">
@@ -100,29 +33,14 @@
         <div class="col-sm-6 text-sm-right text-center">
           <div class="footer_social_container">
             <ul class="footer_social">
-              <li><a href="https://www.facebook.com/blueparrotgames/"><i class="fab fa-facebook-f trans_300"></i></a></li>
-              <li><a href="https://twitter.com/BlueParrotGames"><i class="fab fa-twitter trans_300"></i></a></li>
+              <li><a href="https://www.facebook.com/blueparrotgames/"><img class="ImgFacebook" src="resources/facebook.png"></a></li>
+              <li><a href="https://twitter.com/BlueParrotGames"><img class="ImgTwitter"  src="resources/Twitter.png"></a></li>
+			  <li><a href="https://Youtube"><img class="ImgYoutube"  src="resources/Youtube.png"></a></li>
             </ul>
           </div>
         </div>
       </div>
     </div>
   </footer>
-
-</div>
-
-<script src="js/jquery-3.2.1.min.js"></script>
-<script src="styles/bootstrap4/popper.js"></script>
-<script src="styles/bootstrap4/bootstrap.min.js"></script>
-<script src="plugins/greensock/TweenMax.min.js"></script>
-<script src="plugins/greensock/TimelineMax.min.js"></script>
-<script src="plugins/scrollmagic/ScrollMagic.min.js"></script>
-<script src="plugins/greensock/animation.gsap.min.js"></script>
-<script src="plugins/greensock/ScrollToPlugin.min.js"></script>
-<script src="plugins/progressbar/progressbar.min.js"></script>
-<script src="plugins/OwlCarousel2-2.2.1/owl.carousel.js"></script>
-<script src="plugins/easing/easing.js"></script>
-<script src="js/custom.js"></script>
 </body>
-
 </html>
