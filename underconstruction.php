8fae61312d87f7df024069f9cf64cee45831fec2
rabitibike112 <30289973+rabitibike112@users.noreply.github.com>
2018-11-30 13:07:07 +0200

    Added website icon, changed footer size, font and color and added a hover effect to the link icons
    summary says it all

--- underconstruction.php
+++ underconstruction.php
@@ -2,6 +2,7 @@
 <title>Blue Parrot Games</title>
 <link rel="stylesheet" type="text/css" href="styles/Under_construction.css">
 <link rel="stylesheet" type="text/css" href="styles/responsive.css">
+<link rel="icon" type="image/png" href="/resources/bpg_512.png"/>
 </head>
 <body>
 	<!-- Header -->
@@ -23,24 +24,18 @@
 	  <!-- Footer -->
 	
 	  <footer class="footer">
-    <div class="container">
-      <div class="row footer_content d-flex flex-sm-row flex-column align-items-center">
-        <div class="col-sm-6 cr text-sm-left text-center">
-          <p>
+      <div class="FooterBlackPart">
+          <p class="FooterText">
             This website, is owned and created by BPG. All rights reserverd <br> For more info, feel free to contact grootloops@blueparrotgames.com or tweet @blueparrotgames
           </p>
-        </div>
-        <div class="col-sm-6 text-sm-right text-center">
           <div class="footer_social_container">
             <ul class="footer_social">
               <li><a href="https://www.facebook.com/blueparrotgames/"><img class="ImgFacebook" src="resources/facebook.png"></a></li>
               <li><a href="https://twitter.com/BlueParrotGames"><img class="ImgTwitter"  src="resources/Twitter.png"></a></li>
-			  <li><a href="https://Youtube"><img class="ImgYoutube"  src="resources/Youtube.png"></a></li>
+			  <li><a href="https://www.youtube.com/channel/UCt5Ob_HzkhSjgC1qG7wIhyw?view_as=subscriber"><img class="ImgYoutube"  src="resources/Youtube.png"></a></li>
             </ul>
           </div>
-        </div>
       </div>
-    </div>
   </footer>
 </body>
 </html>
